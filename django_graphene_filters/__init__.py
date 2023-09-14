1971084120b49b841b98b79858973cd3c3a85c48
Rio Weber <riodweber@gmail.com>
2023-09-14 00:49:43 -0400

    publish;

--- django_graphene_filters/__init__.py
+++ django_graphene_filters/__init__.py
@@ -16,7 +16,7 @@ from .filters import (
 )
 from .filterset import AdvancedFilterSet
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 # All classes, methods, part of the public API
 # easier to manage and understand the package
--- tests/test_django_graphene_filters.py
+++ tests/test_django_graphene_filters.py
@@ -5,4 +5,4 @@ from django_graphene_filters import __version__
 
 def test_version() -> None:
     """Test that the version number is correct."""
-    assert __version__ == "0.0.1"
+    assert __version__ == "0.0.5"
