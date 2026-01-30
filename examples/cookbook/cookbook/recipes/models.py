892a8b50389b8c0195f92cd5b90e5d3fd8256575
Rio Weber <riodweber@gmail.com>
2026-01-30 16:23:05 -0500

    Upgrade cookbook start;

--- examples/cookbook/cookbook/recipes/models.py
+++ examples/cookbook/cookbook/recipes/models.py
@@ -7,7 +7,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100)
     instructions = models.TextField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 
