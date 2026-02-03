5f1df6f8400d7b347e830ab99b0ff36141add535
Rio Weber <riodweber@gmail.com>
2026-02-03 13:02:18 -0500

    Update version to 0.1.0

--- django_graphene_filters/__init__.py
+++ django_graphene_filters/__init__.py
@@ -15,7 +15,7 @@ from .filters import (
 )
 from .filterset import AdvancedFilterSet
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 # All classes, methods, part of the public API
 # easier to manage and understand the package
